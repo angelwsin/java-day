a19ea267db729942ce7e343357741a3759fbc908
angelwsin <angelwsin@hotmail.com>
2017-06-23 13:14:14 +0800

    algorithm

---
+++ java-day/java-design-pattern/src/main/java/org/java/design/pattern/App.java
@@ -0,0 +1,13 @@
+package org.java.design.pattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
