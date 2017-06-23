9190ef91f950cfab69724de34297f8bfdb18baef
angelwsin <angelwsin@hotmail.com>
2017-06-23 13:10:33 +0800

    api

---
+++ java-day/java-bio/src/main/java/org/java/bio/App.java
@@ -0,0 +1,13 @@
+package org.java.bio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
