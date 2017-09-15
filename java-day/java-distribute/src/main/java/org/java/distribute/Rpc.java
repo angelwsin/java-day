e59a17fdca91a3546563239a29d40b9143d02322
Administrator <Administrator@AE6ZA7WZKTMUEGM>
2017-09-15 18:22:14 +0800

    日志

---
+++ java-day/java-distribute/src/main/java/org/java/distribute/Rpc.java
@@ -0,0 +1,14 @@
+package org.java.distribute;
+
+public class Rpc {
+    
+    
+    
+    
+    
+    //路由
+    
+    
+    
+
+}
--- java-day/java-logs/src/main/java/org/java/logs/App.java
+++ java-day/java-logs/src/main/java/org/java/logs/App.java
@@ -34,4 +34,30 @@ public class App
         
         System.out.println( "Hello World!" );
     }
+    
+    
+    
+    
+    /*
+     * 梳理下 日志家族
+     * 
+     * 
+     * 
+     *          日志接口         commons-logging(apache)     slf4j(Simple Logging Facade for JAVA )    
+     *          
+     *          实现                 log4j,jdk14logger            logback
+     * 
+     *          运行时              过动态查找的机制，绑定日志实现库                       静态绑定
+     * 
+     * 
+     *                            适配
+     *           
+     *         log4j             slf4j-log4j12                 slf4j
+     *         
+     *         jdk14logger       slf4j-jdk14
+     * 
+     * 
+     * 
+     * 
+     */
 }
