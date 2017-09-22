368b183b0bb201620fb88d43be9f9f3e7474c134
Administrator <Administrator@AE6ZA7WZKTMUEGM>
2017-09-22 18:21:23 +0800

    jdo jpa

---
+++ java-day/java-jdo-jpa/src/main/java/org/java/jdo/jpa/App.java
@@ -0,0 +1,20 @@
+package org.java.jdo.jpa;
+
+/**
+ * jdo,jpa这两者都是java中持久化的规范
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       
+        
+       /* JDO(Java Data Object )是Java对象持久化的新的规范，也是一个用于存取某种数据仓库中的对象的标准化API。JDO提供了透明的对象存储，因此对开发人员来说，存储数据对象完
+        全不需要额外的代码（如JDBC 
+        API的使用）。这些繁琐的例行工作已经转移到JDO产品提供商身上，使开发人员解脱出来，从而集中时间和精力在业务逻辑上。另外，JDO很灵活，因为它
+        可以在任何数据底层上运行。JDBC只是面向关系数据库（RDBMS）JDO更通用，提供到任何数据底层的存储功能，比如关系数据库、文件、XML以及对
+        象数据库（ODBMS）等等，使得应用可移植性更强。
+        JPA全称Java Persistence API.JPA通过JDK 5.0注解或XML描述对象－关系表的映射关系，并将运行期的实体对象持久化到数据库中*/
+    }
+}
