43ccad6c9e8124db2e3c959a3c1c4cb45612a3aa
angelwsin <angelwsin@hotmail.com>
2017-10-17 16:10:15 +0800

    maven-plugins

--- java-day/java-maven-plugins-dev/src/main/java/org/java/maven/plugins/dev/GreetingMojo.java
+++ java-day/java-maven-plugins-dev/src/main/java/org/java/maven/plugins/dev/GreetingMojo.java
@@ -5,6 +5,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 //maven 插件开发   继承AbstractMojo 实现execute方法
 
@@ -19,9 +20,12 @@ import org.apache.maven.plugins.annotations.Mojo;
  */
 @Mojo(name="greeting",defaultPhase=LifecyclePhase.COMPILE)
 public class GreetingMojo extends AbstractMojo{
+    
+    @Parameter
+     private  String userName;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info( "Hello, world." );
+        getLog().info( "Hello, world." +userName);
         
     }
 
