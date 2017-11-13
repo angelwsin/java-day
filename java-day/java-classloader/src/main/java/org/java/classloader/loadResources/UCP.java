4da3cb265b85e5f2aa27d88ea75f6781cbc6f248
angelwsin <angelwsin@hotmail.com>
2017-11-13 17:30:16 +0800

    UrlClasspath

--- java-day/java-classloader/src/main/java/org/java/classloader/loadResources/UCP.java
+++ java-day/java-classloader/src/main/java/org/java/classloader/loadResources/UCP.java
@@ -1,6 +1,7 @@
 package org.java.classloader.loadResources;
 
 import java.lang.reflect.Field;
+import java.net.URL;
 import java.util.List;
 
 import sun.misc.URLClassPath;
@@ -17,9 +18,12 @@ public class UCP {
 		URLClassPath classpath  = (URLClassPath) field.get(classLoader);
 		Field loaders = classpath.getClass().getDeclaredField("loaders");
 		loaders.setAccessible(true);
+		
 		List<?> loadess  = (List<?>) loaders.get(classpath);
 		System.out.println(loadess.size());
-		
+		URL url = classpath.findResource("stylesheet.css", false);
+		System.out.println(url.toString());
+		Class<?> clazz = Class.forName("sun.misc.URLClassPath$JarLoader");
 		
 		
 		
