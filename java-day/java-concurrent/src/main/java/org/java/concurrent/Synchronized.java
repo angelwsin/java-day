eb2b9d886996e2cdd3b058f828113cc2108378cd
wuqiang <wuqiang@wdai.com>
2018-01-03 16:46:58 +0800

    锁

---
+++ java-day/java-concurrent/src/main/java/org/java/concurrent/Synchronized.java
@@ -0,0 +1,13 @@
+package org.java.concurrent;
+
+public class Synchronized {
+	
+	public static void main(String[] args) {
+		/**
+		 * 1.偏向锁       使用对象头
+		 * 2.轻量级锁    cas
+		 * 3.重量级锁    队列排序
+		 */
+	}
+
+}
--- java-day/java-concurrent/src/main/java/org/java/concurrent/共享锁_读锁.java
+++ java-day/java-concurrent/src/main/java/org/java/concurrent/共享锁_读锁.java
@@ -1,5 +1,8 @@
 package org.java.concurrent;
 
 public class 共享锁_读锁 {
+	
+	
+	
 
 }
