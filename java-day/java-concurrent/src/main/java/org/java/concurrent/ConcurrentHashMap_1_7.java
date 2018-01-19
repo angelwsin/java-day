ab7cd09365ef7ca37c2674b99e2689502c98fcd6
wuqiang <wuqiang@wdai.com>
2018-01-19 17:22:03 +0800

    jmm

--- java-day/java-concurrent/src/main/java/org/java/concurrent/ConcurrentHashMap_1_7.java
+++ java-day/java-concurrent/src/main/java/org/java/concurrent/ConcurrentHashMap_1_7.java
@@ -79,11 +79,46 @@ public class ConcurrentHashMap_1_7 {
             table = newTable;
 		 *                  
 		 *       
-		 *               
+		 *           
 		 * 
 		 * 
+		 * jdk1.6 的实现  ConcurrentHashMap 怎么做到读不需要加锁  可以读到写的数据
 		 * 
+		 *  1.在 Segment 维护一个  volatile int count
+		 *  2.HashEntry 中的 volatile V value
+		 *  
+		 *Read operations can thus proceed without locking, but rely
+         * on selected uses of volatiles to ensure that completed
+         * write operations performed by other threads are
+         * noticed. For most purposes, the "count" field, tracking the
+         * number of elements, serves as that volatile variable
+         * ensuring visibility.  This is convenient because this field
+         * needs to be read in many read operations anyway:
+         *
+         *   - All (unsynchronized) read operations must first read the
+         *     "count" field, and should not look at table entries if
+         *     it is 0.
+         *
+         *   - All (synchronized) write operations should write to
+         *     the "count" field after structurally changing any bin.
+         *     The operations must not take any action that could even
+         *     momentarily cause a concurrent read operation to see
+         *     inconsistent data. This is made easier by the nature of
+         *     the read operations in Map. For example, no operation
+         *     can reveal that the table has grown but the threshold
+         *     has not yet been updated, so there are no atomicity
+         *     requirements for this with respect to reads.
 		 */
+		
+	/*ConcurrentHashMap 描述
+
+                支持并发的读和高期望值的写，ConcurrentHashMap 遵守着Hashtable的功能规范
+     ConcurrentHashMap 所有的功能都是线程安全的
+               读操作都不需要加锁
+              支持不需要对整个table加锁进行访问（读写） 分段锁的实现
+
+                检索操作（包括get）通常不会阻塞，因此可能与更新操作（包括put和remove）重叠
+                 不允许null被用作键或值   */ 
 		
 	}
 
---
+++ java-day/java-concurrent/src/main/java/org/java/jmm/JMM.java
@@ -0,0 +1,11 @@
+package org.java.jmm;
+
+
+//http://www.cs.umd.edu/~pugh/java/memoryModel/jsr-133-faq.html
+public class JMM {
+	
+	//Java includes several language constructs, including volatile, final, and synchronized
+	
+	//内存屏障  http://www.rdrop.com/users/paulmck/scalability/paper/whymb.2009.04.05a.pdf
+
+}
